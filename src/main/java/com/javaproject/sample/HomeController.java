6ef1ea8561591d0d6e80355e5d10ed2d6c8bf06c
ryu-zaki <jhonwellryuzaki0987@gmail.com>
2026-04-23 15:37:50 +0800

    prop: Application properties for working environment.

--- src/main/java/com/javaproject/sample/HomeController.java
+++ src/main/java/com/javaproject/sample/HomeController.java
@@ -1,19 +1,24 @@
 package com.javaproject.sample;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    @Value("${spring.application.name}")
+    private String appName;
+
     @RequestMapping("/")
     public String index() {
       String name = getViewName();
+      System.out.println("App Name: " + appName);
       return name;
     }
 
     private String getViewName() {
-        return "index";
+        return "index.html";
     }
 
 }
