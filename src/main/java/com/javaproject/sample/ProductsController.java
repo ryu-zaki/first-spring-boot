69fd795dafd62b5e2beff0b21370923b44c73b9f
ryu-zaki <jhonwellryuzaki0987@gmail.com>
2026-04-24 23:06:20 +0800

    controller: Post request for adding products.

--- src/main/java/com/javaproject/sample/ProductsController.java
+++ src/main/java/com/javaproject/sample/ProductsController.java
@@ -1,18 +1,19 @@
 package com.javaproject.sample;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/products")
 public class ProductsController {
+    private List<Products> products = new ArrayList<Products>();
 
-    @GetMapping("/products")
+    @GetMapping("/")
     public List<Products> getAllProducts() {
-        List<Products> products = new ArrayList<Products>();
+
         products.add(new Products("Milkita", 15));
         products.add(new Products("Peanut Butter", 50.3));
         products.add(new Products("Pandesal", 2.5));
@@ -20,4 +21,9 @@ public class ProductsController {
         return products;
     }
 
+    @PostMapping("/add")
+    public String addProduct(@RequestBody Products newProduct) {
+        products.add(newProduct);
+        return "Product " + newProduct.getProductName() + " with a price of " + newProduct.getPrice() + " has been added";
+    }
 }
