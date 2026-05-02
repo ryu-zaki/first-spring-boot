6db3f505ff9c21b096e97f16a564cb67cda36b15
ryu-zaki <jhonwellryuzaki0987@gmail.com>
2026-05-03 00:40:27 +0800

    entity: creation of Supplier class and utilities.

--- src/main/java/com/javaproject/sample/Supplier.java
+++ src/main/java/com/javaproject/sample/Supplier.java
@@ -2,12 +2,15 @@ package com.javaproject.sample;
 
 public class Supplier {
 
+    private String supplierId;
     private String name;
     private double tax;
 
-    public Supplier(String name, double tax) {
+    public Supplier(String name, double tax, String supplierId) {
         setName(name);
         setTax(tax);
+        setSupplierId(supplierId);
+
     }
 
     public void setName(String name) {
@@ -18,12 +21,20 @@ public class Supplier {
         this.tax = tax;
     }
 
+    public void setSupplierId(String id) {
+        this.supplierId = id;
+    }
+
     public String getName() {
         return name;
     }
 
     public double getTax() {
         return tax;
+    }
+
+    public String getSupplierId() {
+        return supplierId;
     }
 
 }
